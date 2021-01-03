2e870220cc4c5667197f81cf766d11019aec1b81
Haruto Tanbe <harutotanabe09@gmail.com>
2021-01-03 20:06:16 +0900

    fix: 🐛 data fix

--- src/main/resources/db/migration/V1_0_0__create_table.sql
+++
@@ -1,1 +0,0 @@
-create table messages ( id INT not null primary key,messages VARCHAR(255) not null, status VARCHAR );
---
+++ src/main/resources/db/migration/V1__Initial_Setup.sql
@@ -0,0 +1,4 @@
+create table messages (
+    id INT not null primary key,
+    messages VARCHAR(255) not null,
+    status VARCHAR )
