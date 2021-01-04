81e61ef7bfa8b713d12729c3154ae9141132ece4
Haruto Tanbe <harutotanabe09@gmail.com>
2021-01-04 22:30:57 +0900

    feat: 🎸 add icon

--- src/main/java/personal/chatapp/service/GreetingService.java
+++ src/main/java/personal/chatapp/service/GreetingService.java
@@ -3,9 +3,6 @@ package personal.chatapp.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
-import org.springframework.jdbc.core.namedparam.SqlParameterSource;
-import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Service;
 import personal.chatapp.entity.Message;
 
